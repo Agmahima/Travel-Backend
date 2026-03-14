b0f60abfbf0c4505a71072823d64a33135febd17
Mahima Agrawal <agrawalmahima361@gmail.com>
2026-03-14 11:31:17 +0530

    fixed index file

--- index.ts
+++ index.ts
@@ -114,8 +114,8 @@ function serveStatic(app: Express) {
 
 const port = Number(process.env.PORT) || 5000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Booking Service API is running');
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is running on port ${port}`);
 });
   } catch (error) {
     console.log(`Failed to start server: ${(error as Error).message}`);
